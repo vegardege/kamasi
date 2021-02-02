981ce99b8cacb8027df28b1be6e311d0b21b7851
Vegard Egeland <vegardegeland@gmail.com>
2021-02-02 09:20:52 +0100

    Cleanup rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,18 @@ import { terser } from 'rollup-plugin-terser'
 export default [
   {
     input: 'src/index.js',
-    output: {
-      file: 'dist/kamasi.js',
-      format: 'umd',
-      name: 'kamasi',
-    }
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      file: 'dist/kamasi.min.js',
-      format: 'umd',
-      name: 'kamasi',
-    },
-    plugins: [terser()]
+    output: [
+      {
+        file: 'dist/kamasi.js',
+        format: 'umd',
+        name: 'kamasi',
+      },
+      {
+        file: 'dist/kamasi.min.js',
+        format: 'umd',
+        name: 'kamasi',
+        plugins: [terser()],
+      }
+    ]
   }
 ]
